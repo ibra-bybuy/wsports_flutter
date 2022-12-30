35a9d12bd694a0efd65193595f0e9c876504c230
ibragim <i.chekaev@qsoft.ru>
2022-12-30 20:51:51 +0300

    uploading app

---
+++ cmd/upload_app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
+export $(grep -v '^#' .env | xargs)
+
+host=http://localhost:3006/api/v1/app
+file=../build/app/outputs/flutter-apk/app-release.apk
+flutter clean
+flutter pub get
+flutter build apk --release
+curl -v -H "Authorization: Bearer $JWT" -F "file=@$file" $host
