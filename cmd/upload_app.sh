57ac72eecabfb8abdfdc87f26a960b585e2913c2
ibragim <i.chekaev@qsoft.ru>
2022-12-30 20:54:42 +0300

    uploaded bash

--- cmd/upload_app.sh
+++ cmd/upload_app.sh
@@ -7,7 +7,11 @@ export $(grep -v '^#' .env | xargs)
 
 host=http://localhost:3006/api/v1/app
 file=../build/app/outputs/flutter-apk/app-release.apk
+
+cd ".."
 flutter clean
 flutter pub get
 flutter build apk --release
+
+cd "cmd"
 curl -v -H "Authorization: Bearer $JWT" -F "file=@$file" $host
